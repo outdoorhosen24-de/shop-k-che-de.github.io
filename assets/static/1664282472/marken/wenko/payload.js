75e67b7c282c9e050be3aa3c62e93ce425bef735
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-09-27 12:42:13 +0000

    deploy: c0ed143c889313c7c5b2c75446b31ea52893e33a

---
+++ assets/static/1664282472/marken/wenko/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/marken/wenko", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/wenko-wandblende-edelstahl-wenko-3892817-1.jpg":"\u002Fassets\u002Fimages\u002Fwenko-wandblende-edelstahl-wenko-3892817-1.webp"}}],fetch:{},mutations:void 0});
